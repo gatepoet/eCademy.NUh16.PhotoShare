f7be759cd0eff382ff34df46306bbf7c4e592f16
kristoffer.deinoff <kristoffer.deinoff@gmail.com>
2018-02-13 14:45:12 +0100

    Added CORS to website

--- eCademy.NUh16.PhotoShare/App_Start/WebApiConfig.cs
+++ eCademy.NUh16.PhotoShare/App_Start/WebApiConfig.cs
@@ -8,6 +8,7 @@ namespace eCademy.NUh16.PhotoShare
         public static void Register(HttpConfiguration config)
         {
             // Web API configuration and services
+            config.EnableCors();
 
             // Web API routes
             config.MapHttpAttributeRoutes();
