89a338b4582450835fe2a17de593d4d4e4d047fd
kristoffer.deinoff <kristoffer.deinoff@gmail.com>
2018-02-18 16:10:11 +0100

    Use scalable images for icons

--- eCademy.NUh16.PhotoShare.Droid/Resources/Resource.Designer.cs
+++ eCademy.NUh16.PhotoShare.Droid/Resources/Resource.Designer.cs
@@ -2507,26 +2507,26 @@ namespace eCademy.NUh16.PhotoShare.Droid
 			// aapt resource value: 0x7f020053
 			public const int avd_hide_password = 2130837587;
 			
-			// aapt resource value: 0x7f0200a7
-			public const int avd_hide_password_1 = 2130837671;
-			
 			// aapt resource value: 0x7f0200a8
-			public const int avd_hide_password_2 = 2130837672;
+			public const int avd_hide_password_1 = 2130837672;
 			
 			// aapt resource value: 0x7f0200a9
-			public const int avd_hide_password_3 = 2130837673;
+			public const int avd_hide_password_2 = 2130837673;
+			
+			// aapt resource value: 0x7f0200aa
+			public const int avd_hide_password_3 = 2130837674;
 			
 			// aapt resource value: 0x7f020054
 			public const int avd_show_password = 2130837588;
 			
-			// aapt resource value: 0x7f0200aa
-			public const int avd_show_password_1 = 2130837674;
-			
 			// aapt resource value: 0x7f0200ab
-			public const int avd_show_password_2 = 2130837675;
+			public const int avd_show_password_1 = 2130837675;
 			
 			// aapt resource value: 0x7f0200ac
-			public const int avd_show_password_3 = 2130837676;
+			public const int avd_show_password_2 = 2130837676;
+			
+			// aapt resource value: 0x7f0200ad
+			public const int avd_show_password_3 = 2130837677;
 			
 			// aapt resource value: 0x7f020055
 			public const int com_facebook_auth_dialog_background = 2130837589;
@@ -2700,79 +2700,82 @@ namespace eCademy.NUh16.PhotoShare.Droid
 			public const int ic_file_upload_white = 2130837645;
 			
 			// aapt resource value: 0x7f02008e
-			public const int logo = 2130837646;
+			public const int ic_file_upload_white_48dp = 2130837646;
 			
 			// aapt resource value: 0x7f02008f
-			public const int messenger_bubble_large_blue = 2130837647;
+			public const int logo = 2130837647;
 			
 			// aapt resource value: 0x7f020090
-			public const int messenger_bubble_large_white = 2130837648;
+			public const int messenger_bubble_large_blue = 2130837648;
 			
 			// aapt resource value: 0x7f020091
-			public const int messenger_bubble_small_blue = 2130837649;
+			public const int messenger_bubble_large_white = 2130837649;
 			
 			// aapt resource value: 0x7f020092
-			public const int messenger_bubble_small_white = 2130837650;
+			public const int messenger_bubble_small_blue = 2130837650;
 			
 			// aapt resource value: 0x7f020093
-			public const int messenger_button_blue_bg_round = 2130837651;
+			public const int messenger_bubble_small_white = 2130837651;
 			
 			// aapt resource value: 0x7f020094
-			public const int messenger_button_blue_bg_selector = 2130837652;
+			public const int messenger_button_blue_bg_round = 2130837652;
 			
 			// aapt resource value: 0x7f020095
-			public const int messenger_button_send_round_shadow = 2130837653;
+			public const int messenger_button_blue_bg_selector = 2130837653;
 			
 			// aapt resource value: 0x7f020096
-			public const int messenger_button_white_bg_round = 2130837654;
+			public const int messenger_button_send_round_shadow = 2130837654;
 			
 			// aapt resource value: 0x7f020097
-			public const int messenger_button_white_bg_selector = 2130837655;
+			public const int messenger_button_white_bg_round = 2130837655;
 			
 			// aapt resource value: 0x7f020098
-			public const int navigation_empty_icon = 2130837656;
+			public const int messenger_button_white_bg_selector = 2130837656;
 			
 			// aapt resource value: 0x7f020099
-			public const int notification_action_background = 2130837657;
+			public const int navigation_empty_icon = 2130837657;
 			
 			// aapt resource value: 0x7f02009a
-			public const int notification_bg = 2130837658;
+			public const int notification_action_background = 2130837658;
 			
 			// aapt resource value: 0x7f02009b
-			public const int notification_bg_low = 2130837659;
+			public const int notification_bg = 2130837659;
 			
 			// aapt resource value: 0x7f02009c
-			public const int notification_bg_low_normal = 2130837660;
+			public const int notification_bg_low = 2130837660;
 			
 			// aapt resource value: 0x7f02009d
-			public const int notification_bg_low_pressed = 2130837661;
+			public const int notification_bg_low_normal = 2130837661;
 			
 			// aapt resource value: 0x7f02009e
-			public const int notification_bg_normal = 2130837662;
+			public const int notification_bg_low_pressed = 2130837662;
 			
 			// aapt resource value: 0x7f02009f
-			public const int notification_bg_normal_pressed = 2130837663;
+			public const int notification_bg_normal = 2130837663;
 			
 			// aapt resource value: 0x7f0200a0
-			public const int notification_icon_background = 2130837664;
-			
-			// aapt resource value: 0x7f0200a5
-			public const int notification_template_icon_bg = 2130837669;
-			
-			// aapt resource value: 0x7f0200a6
-			public const int notification_template_icon_low_bg = 2130837670;
+			public const int notification_bg_normal_pressed = 2130837664;
 			
 			// aapt resource value: 0x7f0200a1
-			public const int notification_tile_bg = 2130837665;
+			public const int notification_icon_background = 2130837665;
+			
+			// aapt resource value: 0x7f0200a6
+			public const int notification_template_icon_bg = 2130837670;
+			
+			// aapt resource value: 0x7f0200a7
+			public const int notification_template_icon_low_bg = 2130837671;
 			
 			// aapt resource value: 0x7f0200a2
-			public const int notify_panel_notification_icon_bg = 2130837666;
+			public const int notification_tile_bg = 2130837666;
 			
 			// aapt resource value: 0x7f0200a3
-			public const int tooltip_frame_dark = 2130837667;
+			public const int notify_panel_notification_icon_bg = 2130837667;
 			
 			// aapt resource value: 0x7f0200a4
-			public const int tooltip_frame_light = 2130837668;
+			public const int tooltip_frame_dark = 2130837668;
+			
+			// aapt resource value: 0x7f0200a5
+			public const int tooltip_frame_light = 2130837669;
 			
 			static Drawable()
 			{
